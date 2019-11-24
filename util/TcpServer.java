387df0b7e0b285afb14f61945baa448f8bf3a9b6
shudal <shudal@qq.com>
2019-11-24 17:39:14 +0800

    first commit

--- util/TcpServer.java
+++ util/TcpServer.java
@@ -23,19 +23,16 @@ public class TcpServer {
     public int threadCount = 0;
     public ThreadGroup clientThreadGroup;
     public ArrayList<ServerThread> clientThreads = new ArrayList<>();
-    public static GameMapStr gameMapStr;
     public SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     public TcpServer(int port) {
         connectPort = port;
         long nowTimeLong = System.currentTimeMillis() / 1000;
-        gameMapStr = new GameMapStr(nowTimeLong, "2,3.333002,1.74068,358.1939|1,-11.54934,-2.688283,3.479636|");
         init();
         listen();
     }
     public void init() {
         try {
             serverSocket = new ServerSocket(connectPort);
-            clientThreadGroup = new ThreadGroup("clients");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -52,56 +49,82 @@ public class TcpServer {
             e.printStackTrace();
         }
     }
-    public void sendAll(String msg) {
+    public void myRemove(String threadName) {
+        new Thread(()->{
+            System.out.println(threadName);
+            Iterator<ServerThread> it = clientThreads.iterator();
+            while (it.hasNext()) {
+                if (it.next().threadName.equals(threadName)) {
+                    it.remove();
+                    System.out.println("移除threadName=" + threadName);
+                    System.out.println("        ,roomid=" + it.next().roomid);
+                    break;
+                }
+            }
+        });
+    }
+    public void sendAll(String roomid, String threadName, String msg) {
         new Thread(()-> {
             for (ServerThread aThread : clientThreads) {
-                aThread.sendMsgStr(msg);
+                if (roomid.equals(aThread.roomid) && !threadName.equals(aThread.threadName)) {
+                    aThread.sendMsgStr(msg);
+                }
             }
         }).start();
     }
-    public void handleMsg(MyMsg mymsg) {
-        if (mymsg.type == CodeConfig.TYPE_UPLOAD_MY_MAP) {
-            long nowTimeLong = System.currentTimeMillis() / 1000;
-            if (nowTimeLong > gameMapStr.timestamp) {
-                gameMapStr.timestamp = nowTimeLong;
-                gameMapStr.mapStr = mymsg.msg;
-                System.out.println("存储game map");
-            }
-        } else if (mymsg.type == CodeConfig.TYPE_NEW_CLIENT) {
-            sendAll(new MyMsg(CodeConfig.SERVER_PLAYER_ID, CodeConfig.TYPE_UPDATE_MAP, gameMapStr.mapStr).toString());
-        }
-    }
     protected class ServerThread extends Thread {
         Socket s;
+        public String roomid;
         public BufferedReader in;
         public PrintWriter out;
+        public String threadName;
         public ServerThread(ThreadGroup _threadGroup, String _threadName, Socket _s) {
             super(_threadGroup, _threadName);
             s = _s;
+            roomid = null;
+            threadName = _threadName;
         }
         public void sendMsgStr(String msgStr) {
             out.println(msgStr);
             out.flush();
+        }
+        public void myclose() {
+            try {
+                in.close();
+                out.close();
+                s.close();
+                myRemove(threadName);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         public void run() {
             try {
                 in = new BufferedReader(new InputStreamReader(s.getInputStream()));
                 out = new PrintWriter(s.getOutputStream());
                 String msg;
-                while (!(msg = in.readLine()).equals("quit")) {
+                while (true) {
+                    msg = in.readLine();
+                    if (msg.equals("quit")) {
+                        myclose();
+                        break;
+                    }
                     String nowTimeStr = df.format(new Date());
                     System.out.println(nowTimeStr + "#\n    " + msg);
-                    sendAll(msg);
-                    if (msg.charAt(0) != '{') {
-                        handleMsg(new MyMsg(msg));
+                    if (roomid == null) {
+                        int l = msg.length();
+                        StringBuilder sb = new StringBuilder();
+                        for (int i=0; msg.charAt(i) != '|'; ++i) {
+                            sb.append(msg.charAt(i));
+                        }
+                        roomid = sb.toString();
                     }
+                    sendAll(roomid, threadName, msg);
                 }
-                in.close();
-                out.close();
-                s.close();
-
+                myclose();
             } catch (Exception e) {
                 e.printStackTrace();
+                myclose();
             }
         }
     }
